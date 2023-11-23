12f58f6452a77a20758a65885b1e0fcb7f7c4fce
Max_Shulga <shulgamackim@gmail.com>
2023-11-23 14:51:02 +0200

    timer done

--- src/js/main.bundle.js
+++ src/js/main.bundle.js
@@ -1,3 +1,42 @@
-// Rest of your JavaScript code
-console.log('Hello, World!')
-console.log(123)
+import moment from 'moment'
+
+const startButton = document.getElementById('start-button')
+
+startButton.addEventListener('click', () => {
+	const startTime = moment()
+	const timerHours = +document.getElementById('hours').value
+	const timerMinutes = +document.getElementById('minutes').value
+	const timerSeconds = +document.getElementById('seconds').value
+	const endTime = moment()
+		.local()
+		.add(timerHours, 'hours')
+		.add(timerMinutes, 'minutes')
+		.add(timerSeconds, 'second')
+
+	let timerDuration = moment.duration(endTime.diff(startTime))
+	console.log(timerDuration)
+	const format = 'HH:mm:ss'
+	const timer = document.getElementById('timer-ongoing-clock')
+
+	const interval = setInterval(() => {
+		timer.textContent = moment
+			.utc(moment.duration(timerDuration).asMilliseconds())
+			.format(format)
+
+		timerDuration -= 1000
+		if (+timerDuration < 0) {
+			clearInterval(interval)
+			document.getElementById('timer-sound').play()
+		}
+
+		setTimeout(() => {
+			timer.style.display = 'flex'
+			document.getElementById('start-button').style.display = 'none'
+			document
+				.getElementsByClassName('timer-text')[0]
+				.classList.add('timer-text-ongoing')
+			document.getElementById('timer-clocks-container').style.display = 'none'
+			document.getElementById('timer-ongoing').style.display = 'flex'
+		}, 0)
+	}, 1000)
+})
